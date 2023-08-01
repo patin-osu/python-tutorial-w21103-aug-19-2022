8ad895b0c424b71c62de571104324f66e612644b
patinosu <25068193-patinosu@users.noreply.replit.com>
2023-08-01 06:07:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Import
+from turtle import *
+
+#1
+print("ภฑิล เมืองจันทร์")
+print("อายุ 11 ปี")
+print("สตรีสมุทรปราการ")
+print("ตุลย์")
+
+#2
+print(2565 - 543)
+
+#3
+shape("turtle")
+speed(1)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
